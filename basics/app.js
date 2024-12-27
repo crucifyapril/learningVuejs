301a54ca690bbca15e32fc771ea40d7bcce73b18
crucifyapril <sayaf3@yahoo.com>
2024-12-27 22:16:44 +0300

    Добавить заметки в список Vue

---
+++ basics/app.js
@@ -0,0 +1,29 @@
+const App = {
+    data() {
+        return {
+            title: 'Список заметок',
+            placeholderString: 'Введите название заметки',
+            inputValue: '',
+            notes: []
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        deleteNote(index) {
+            this.notes.splice(index, 1)
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
